ba22aabcf1329d3cf28d73545b3f0fc0fe02f3b9
Colin Wren <colin@gimpneek.com>
2013-01-26 15:49:48 +0000

    adding sitemap check and link tag collection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import urllib2
 from pprint import pprint
+import lxml.html
 
 def getHeaders(pageObj):
     return {
@@ -10,14 +11,36 @@ def getHeaders(pageObj):
 
 def checkFile(url):
     fileBool = False
+    fileText = ""
     try:
         fileCall = urllib2.urlopen(url)
+        fileText = fileCall.read()
         fileBool = True
     except urllib2.HTTPError, e:
         fileBool = False
     except urllib2.URLError, e:
         fileBool = False
-    return fileBool
+    return {'present': fileBool, 'contents' : fileText}
+
+def checkLinkTags(siteCall):
+    pageHTML = lxml.html.fromstring(siteCall.read())
+    linkTags = pageHTML.cssselect('link')
+    links = {}
+    for link in linkTags:
+       links.setdefault(link.attrib['rel'].decode(),[]).append(link.attrib['href'].decode())
+    return links
+
+def checkSiteMap(robots, links):
+    siteMapBool = False
+    if 'sitemap' in links:
+        siteMapBool = True
+    else:
+        try:
+            robotsText = robots
+            siteMapBool = 'Sitemap' in robotsText
+        except urllib2.HTTPError, e:
+            siteMapBool = False
+    return siteMapBool
 
 def getSsl(site, homePageObj):
     if site['site-url'].startswith("https:"):
@@ -35,10 +58,14 @@ siteFile = open('test-data.json');
 sites = json.load(siteFile)
 for site in sites:
     homePageObj = urllib2.urlopen(site['site-url'])
+    robots = checkFile(site['site-url'] + '/robots.txt')
+    humans = checkFile(site['site-url'] + '/humans.txt')
+    links = checkLinkTags(homePageObj)
 
     site['headers'] = getHeaders(homePageObj)
-    site['robots'] = checkFile(site['site-url'] + '/robots.txt')
-    site['humans'] = checkFile(site['site-url'] + '/humans.txt')
+    site['robots'] = robots['present']
+    site['sitemap'] = checkSiteMap(robots['contents'], links)
+    site['humans'] = humans['present']
     site['ssl'] = getSsl(site, homePageObj)
 
     # Print site on screen. TODO: Save this info to a file
