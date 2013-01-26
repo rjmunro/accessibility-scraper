ffb3674ba47a8b2acbe1b6fd4a1542ca55b020f8
Mariana <mariana@whiteoctober.co.uk>
2013-01-26 22:14:48 +0000

    html and css tidying up

---
+++ site/js/graphs/graphtest.js
@@ -0,0 +1,51 @@
+$(function () {
+    var chart;
+    
+    $(document).ready(function () {
+    	
+    	// Build the chart
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+        	    pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+            	percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Browser share',
+                data: [
+                    ['Dont support mobile',   45.0],
+                    ['Uses media queries',       26.8],
+                    {
+                        name: 'Chrome',
+                        y: 12.8,
+                        sliced: true,
+                        selected: true
+                    },
+                    ['Has a mobile version of the site',    8.5],
+                    ['Another example',     6.2],
+                    ['Others',   0.7]
+                ]
+            }]
+        });
+    });
+    
+});
