16c1b6ac2810fcf9ace4da510ab8f08eae0facc0
Colin Wren <colin@gimpneek.com>
2013-01-26 14:22:16 +0000

    adding checkfile for robots.txt & humans.txt

--- main.py
+++ main.py
@@ -8,13 +8,24 @@ def getHeaders(pageObj):
         'server': pageObj.info().getheader('Server')
     }
 
-siteFile = open('all-sites.json');
-#siteFile = open('test-data.json');
+def checkFile(url):
+    fileBool = False
+    try:
+        fileCall = urllib2.urlopen(url)
+        fileBool = True
+    except urllib2.HTTPError, e:
+        fileBool = False
+    return fileBool
+
+#siteFile = open('all-sites.json');
+siteFile = open('test-data.json');
 sites = json.load(siteFile)
 for site in sites:
     homePageObj = urllib2.urlopen(site['site-url'])
 
     site['headers'] = getHeaders(homePageObj)
+    site['robots'] = checkFile(trust['trust-url'] + '/robots.txt')
+    site['humans'] = checkFile(trust['trust-url'] + '/humans.txt')
 
     # Print site on screen. TODO: Save this info to a file
     pprint(site)
