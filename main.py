b805aca26dd14b60259e20a86647e4d55158266d
Colin Wren <colin@gimpneek.com>
2013-01-26 13:37:03 +0000

    Initial commit of basic starting script

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+import urllib2
+from pprint import pprint
+
+def getHeaders(pageObj):
+    return {
+        'poweredBy': pageObj.info().getheader('X-powered-by'),
+        'server': pageObj.info().getheader('Server')
+    }
+
+siteFile = open('ScraperWiki - Trust URLs.json');
+sites = json.load(siteFile)
+for site in sites:
+    homePageObj = urllib2.urlopen(site['site-url'])
+
+    site['headers'] = getHeaders(homePageObj)
+
+    # Print site on screen. TODO: Save this info to a file
+    pprint(site)
+
+siteFile.close()
+
+
