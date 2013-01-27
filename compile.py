8864793fec9e7cf2d215f75a6d515e2b553914d0
Robert (Jamie) Munro <rjmunro@arjam.net>
2013-01-27 12:53:33 +0000

    Compile servers and frameworks by version by splitting at /

--- compile.py
+++ compile.py
@@ -18,8 +18,10 @@ robotsTrue = 0
 robotsFalse = 0
 humansTrue = 0
 doctypes = {}
-servers = {}
-frameworks = {}
+serversSummary = {}
+frameworksSummary = {}
+serversFull = {}
+frameworksFull = {}
 sitemapTrue = 0
 sslTrue = 0
 siteList = {}
@@ -36,14 +38,29 @@ for infile in listing:
     if siteJSON['humans'] == True:
         humansTrue+=1
     doctypes[siteJSON['doctype']] = doctypes.setdefault(siteJSON['doctype'],0) + 1
-    servers[siteJSON['headers']['server']] = servers.setdefault(siteJSON['headers']['server'],0) + 1
-    frameworks[siteJSON['headers']['poweredBy']] = frameworks.setdefault(siteJSON['headers']['poweredBy'],0) + 1
+    serversFull[siteJSON['headers']['server']] = serversFull.setdefault(siteJSON['headers']['server'],0) + 1
+    frameworksFull[siteJSON['headers']['poweredBy']] = frameworksFull.setdefault(siteJSON['headers']['poweredBy'],0) + 1
+    serverSummary = siteJSON['headers']['server'] and siteJSON['headers']['server'].split('/')[0]
+    serversSummary[serverSummary] = serversSummary.setdefault(serverSummary, 0) + 1
+    frameworkSummary = siteJSON['headers']['poweredBy'] and siteJSON['headers']['poweredBy'].split('/')[0]
+    frameworksSummary[frameworkSummary] = frameworksSummary.setdefault(frameworkSummary,0) + 1
     if siteJSON['sitemap'] == True:
         sitemapTrue+=1
     if siteJSON['ssl'] == 'yes':
         sslTrue+=1
 #create JSONfile
-overJSON = {'robotsTrue':robotsTrue,'robotsFalse':robotsFalse,'humansTrue':humansTrue,'doctypes':doctypes,'servers':servers,'frameworks':frameworks,'sitemap':sitemapTrue,'ssl':sslTrue}
+overJSON = {
+    'robotsTrue':robotsTrue,
+    'robotsFalse':robotsFalse,
+    'humansTrue':humansTrue,
+    'doctypes':doctypes,
+    'servers':serversSummary,
+    'frameworks':frameworksSummary,
+    'serversFull':serversFull,
+    'frameworksFull':frameworksFull,
+    'sitemap':sitemapTrue,
+    'ssl':sslTrue
+}
 #print
 outfile = io.open('overview.json', 'wb')
 json.dump(overJSON, outfile, sort_keys = True, indent=4, separators=(',', ': '))
